c9be5701ea389b8bbe5591eac18425404ef320a3
thisIsTheFoxe <thisisthefoxe@gmail.com>
2020-05-26 10:42:19 +0200

    made some minor adjustments

--- tests/iris.py
+++ tests/iris.py
@@ -2,9 +2,10 @@ from DMML.NeuralNetwork import NeuralNetwork
 import pandas as pd
 import numpy as np
 
-# bad/dirty data
+#import data
 data = pd.read_csv('tests/iris.csv')
 
+#split data
 dA = data[:50].sample(frac=1)
 dB = data[50:100].sample(frac=1)
 dC = data[100:].sample(frac=1)
@@ -12,7 +13,7 @@ dC = data[100:].sample(frac=1)
 trainingData = pd.concat([dA[5:],dB[5:],dC[5:]]).sample(frac=1)
 testData = pd.concat([dA[:5],dB[:5],dC[:5]]).sample(frac=1)
 
-print(dA, testData)
+# print(testData)
 
 def formatResult(name):
     if name == "Setosa": return [1,0,0]
@@ -20,7 +21,6 @@ def formatResult(name):
     elif name == "Virginica": return [0,0,1]
     else: return [0,0,0]
 
-# print(train.loc[1][0:-1])
 
 print("- NeuralNetwork")
 nn = NeuralNetwork(4, 10, 50, 3)
@@ -28,11 +28,9 @@ nn = NeuralNetwork(4, 10, 50, 3)
 print("\n TRAIN \n")
 for ix, row in trainingData.iterrows():
     input = row[0:-1].apply(pd.to_numeric, errors='coerce')
-    # print(input, formatResult(row[-1]))
     nn.train([input], formatResult(row[-1]))
 print("\n TEST \n")
 
 for ix, row in testData.iterrows():
-    # print(row)
     result = nn.predict([row[0:-1]])
     print(result, "<>", formatResult(row[-1]))
